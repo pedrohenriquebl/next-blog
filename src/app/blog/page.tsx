b92929d4aa68be8a8c341a1e3c8dcd1e9a04628e
Pedro Henrique <pedruhbl@yahoo.com.br>
2025-10-03 11:28:06 -0300

    :chore: add seo in blog list page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,28 @@
-import { BlogList } from "@/templates/blog";
+import type { Metadata } from "next";
 import { allPosts } from "contentlayer/generated";
+import { BlogList } from "@/templates/blog";
+
+export const metadata: Metadata = {
+    title: 'Blog',
+    description: 'Dicas e estratégias para impulsionar seu negócio',
+    robots: 'index, follow',
+    openGraph: {
+        title: 'Blog',
+        description: 'Dicas e estratégias para impulsionar seu negócio',
+        url: 'https://ph-next-blog.vercel.app/og-image.jpg',
+        siteName: 'Site.set',
+        locale: 'pt_BR',
+        type: 'website',
+        images: [
+            {
+                url: 'https://ph-next-blog.vercel.app/og-image.jpg',
+                width: 800,
+                height: 600,
+                alt: 'Site.set'
+            }
+        ]
+    }
+}
 
 export default function BlogListPage() {
     const sortedPosts = allPosts.sort(
